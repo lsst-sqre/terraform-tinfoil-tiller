56f0e18e93708439dc0204789e7cd398728977e3
Dominik Schröter <dschroter@bethel.jw.org>
2019-01-15 22:32:30 +0100

    Use real output instead of variables
    This enhances dependency management by terraform.
    
    It will cause terraform to delete the helm setup after other
    helm resources were deleted.

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "namespace" {
   description = "kubernetes namespace in which tiller is installed"
   sensitive   = false
-  value       = "${var.namespace}"
+  value       = "${kubernetes_deployment.tiller_deploy.metadata.0.namespace}"
 }
 
 output "service_account" {
   description = "name of kubernetes service account for tiller"
   sensitive   = false
-  value       = "${var.service_account}"
+  value       = "${kubernetes_deployment.tiller_deploy.spec.0.template.0.spec.0.service_account_name}"
 }
